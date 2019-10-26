7965eb0d8f3c8acb7a1f3e1515e17c7e98a9ed10
Martin Toman <whomenope@outlook.com>
2019-10-27 00:35:07 +0200

    Pass faces to hierarchy builder

--- src/MeshHierarchy.hpp
+++ src/MeshHierarchy.hpp
@@ -5,18 +5,40 @@
 #include "intersect.hpp"
 
 class MeshHierarchy {
+private:
+  static const int MAX_DEPTH = 2;
+  static const int MIN_TRIANGLES = 5;
+
+  typedef struct BoundingBox {
+    int level;
+
+    Eigen::Vector3f min;
+    Eigen::Vector3f max;
+
+    std::vector<int> *faces;
+
+    BoundingBox *less;
+    BoundingBox *more;
+  } BoundingBox;
+
 public:
   MeshHierarchy() {}
   MeshHierarchy(const Tucano::Mesh &mesh) {
     this->mesh = mesh;
-    buildUp();
+
+    int num_faces = this->mesh.getNumberOfFaces();
+    std::vector<int> *faces = new std::vector<int>();
+    for (int i = 0; i < num_faces; i++) {
+      faces->push_back(i);
+    }
+
+    buildUp(this->box, 0, faces);
   }
 
   bool intersect(const Eigen::Vector3f &origin,
                  const Eigen::Vector3f &point,
                  Tucano::Face **outFace,
                  Eigen::Vector3f **outIntersect) {
-
     Eigen::Affine3f shapeMatrix = this->mesh.getShapeMatrix();
 
     if (!Intersect::box(origin, point, shapeMatrix * this->box->min, shapeMatrix * this->box->max))
@@ -60,16 +82,21 @@ public:
   }
 
 protected:
-  void buildUp() {
+  void buildUp(BoundingBox *box, int level, std::vector<int>* faces) {
+    if (level >= MAX_DEPTH)
+      return;
+
     box = new BoundingBox();
+    box->level = level;
+    box->faces = faces;
 
     constexpr float minf = std::numeric_limits<float>::min();
     constexpr float maxf = std::numeric_limits<float>::max();
     box->min = Eigen::Vector3f(maxf, maxf, maxf);
     box->max = Eigen::Vector3f(minf, minf, minf);
 
-    int num_faces = mesh.getNumberOfFaces();
-    for (int i = 0; i < num_faces; ++i) {
+    // get box bounds
+    for (int i : *faces) {
       Tucano::Face face = mesh.getFace(i);
       // Assume a triangle
       Eigen::Vector4f vert1 = mesh.getVertex(face.vertex_ids[0]);
@@ -80,10 +107,32 @@ protected:
       updateMinMax(&(box->min), &(box->max), &vert2);
       updateMinMax(&(box->min), &(box->max), &vert3);
     }
+
+    // get split axis
+    float xspan = fabs(box->min.x() - box->max.x());
+    float yspan = fabs(box->min.y() - box->max.y());
+    float zspan = fabs(box->min.z() - box->max.z());
+    float span = std::max(xspan, std::max(yspan, zspan));
+
+    std::cout << box->min << std::endl << box->max << std::endl;
+
+    std::vector<int> *less = new std::vector<int>();
+    std::vector<int> *more = new std::vector<int>();
+
+    if (xspan == span) {
+
+    }
+    else if (yspan == span) {
+
+    }
+    else if (zspan == span) {
+
+    }
   }
 
 private:
-  void updateMinMax(Eigen::Vector3f *min, Eigen::Vector3f *max, Eigen::Vector4f *vertex) {
+  void updateMinMax(Eigen::Vector3f *min, Eigen::Vector3f *max,
+      Eigen::Vector4f *vertex) {
     if (vertex->x() > max->x())
       (*max)(0) = vertex->x();
     if (vertex->y() > max->y())
@@ -101,14 +150,5 @@ private:
 
   Tucano::Mesh mesh;
 
-  typedef struct BoundingBox {
-    Eigen::Vector3f min;
-    Eigen::Vector3f max;
-
-    std::vector<int> faces;
-
-    BoundingBox *left;
-    BoundingBox *right;
-  } BoundingBox;
   BoundingBox* box;
 };
